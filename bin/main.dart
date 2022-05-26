77dccaffc60bdd5aa850c290d2129e6a2f48b5ec
Vanessa Oliveira <vanessaoliveira2706@gmail.com>
2022-05-26 16:24:10 -0300

    refactoring example

--- bin/main.dart
+++ bin/main.dart
@@ -1,21 +1,56 @@
-
-import 'dart:convert';
 import 'dart:io';
 
-import 'package:desafio01/models/main.dart';
+import 'package:desafio01/models/car.dart';
+import 'package:desafio01/models/driver.dart';
 
 void main() {
-  print('Voce deseja realizar uma corrida?');
-  readLine().listen(processLine);
+  print('Voce deseja realizar uma corrida? Sim ou Não');
+  String? answer = stdin.readLineSync();
+  initRide(answer);
 }
 
-Stream<String> readLine() {
-  // ignore: prefer_typing_uninitialized_variables
-  var utf8;
-  return stdin.transform(utf8.decoder).transform(const LineSplitter());
+void initRide(String? answer) {
+  if (answer == 'Sim' || answer == 'S') {
+    loading('Buscando Motorista! Aguarde...');
+    findDriver();
+    print('Corrida iniciada');
+    loading('"Dirigindo................."');
+    print('Chegamos no seu destino! Até mais!');
+    exit(0);
+  } else if (answer == 'Nao' || answer == 'N') {
+    exit(0);
+  } else {
+    print('Respota Inválida!');
+    main(); // RECURSÃOOOO
+  }
 }
 
-void processLine(String line) {
-  List<String> inputs = line.split(' ');
-  init(inputs[0]);
+void loading(String msg) {
+  for (var count = 1; count <= 3; count++) {
+    print(msg);
+    delay(3);
+  }
+}
+
+void delay(int seconds) {
+  sleep(Duration(seconds: seconds));
+}
+
+void findDriver() {
+  Car carro = Car(name: 'Argo', brand: 'Fiat', color: 'azul', id: 'HGI-3245');
+
+  Driver motorista =
+      Driver(car: carro, name: 'Edson', rate: 4.9, ridesNumber: 1000);
+
+  printDriverInfo(motorista);
+}
+
+void printDriverInfo(Driver motorista) {
+  print(
+      '''Seu Motorista é: ${motorista.name}, \n
+         Avaliação: ${motorista.rate},\n
+         Número de corridas: ${motorista.ridesNumber}, \n
+         Carro: ${motorista.car.name}, ${motorista.car.brand}, ${motorista.car.color}, \n
+         Placa: ${motorista.car.id}, 
+    ''');
 }
